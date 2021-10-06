d79cc7e9e780a0db7e6f35c6af1800d9070084d3
Szymon Sulejczak <szymonsulejczak@sitero.net>
2021-10-06 19:14:01 +0200

    ✨ Add projects cards

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -12,4 +12,30 @@ export class ProjectsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  cards: any[] = [
+    {
+      title: 'e-commerce app',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor tellus vitae lobortis condimentum. Fusce efficitur eleifend quam id interdum. Ut nec lorem sapien. Nunc id magna aliquam, hendrerit diam eget, facilisis leo. Suspendisse massa ante, lobortis quis ex nec, laoreet tincidunt sem. Nam lobortis porta elit, sed tempor tortor laoreet eu.',
+      code: '#',
+      app: '#',
+      yt: '#',
+      tags: ['TypeScript', 'ASP.NET Core', 'C#', 'Angular', 'MySQL', 'SCSS'],
+    },
+    {
+      title: 'chat app',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor tellus vitae lobortis condimentum. Fusce efficitur eleifend quam id interdum. Ut nec lorem sapien. Nunc id magna aliquam, hendrerit diam eget, facilisis leo. Suspendisse massa ante, lobortis quis ex nec, laoreet tincidunt sem. Nam lobortis porta elit, sed tempor tortor laoreet eu.',
+      code: '#',
+      app: '#',
+      yt: '#',
+      tags: ['TypeScript', 'ASP.NET Core', 'C#', 'Angular', 'MySQL', 'SCSS'],
+    },
+    {
+      title: 'portfolio',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor tellus vitae lobortis condimentum. Fusce efficitur eleifend quam id interdum. Ut nec lorem sapien. Nunc id magna aliquam, hendrerit diam eget, facilisis leo. Suspendisse massa ante, lobortis quis ex nec, laoreet tincidunt sem. Nam lobortis porta elit, sed tempor tortor laoreet eu.',
+      code: '#',
+      app: '#',
+      yt: '#',
+      tags: ['TypeScript', 'ASP.NET Core', 'C#', 'Angular', 'MySQL', 'SCSS'],
+    },
+  ];
 }
