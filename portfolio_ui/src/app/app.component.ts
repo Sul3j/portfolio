b90e1cba69533fec1b11e93b3fbf2cc7ffe03b9e
Szymon Sulejczak <contact@szymonsulejczak.com>
2025-02-19 16:13:56 +0100

    add navigation structure

--- portfolio_ui/src/app/app.component.ts
+++ portfolio_ui/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ portfolio_ui/src/app/navigation/navigation.component.ts
@@ -0,0 +1,30 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  imports: [],
+  templateUrl: './navigation.component.html',
+  styleUrl: './navigation.component.scss'
+})
+export class NavigationComponent {
+  isMenuOpen = false;
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.navbar__mobile') && !target.closest('.navbar__hamburger') && !target.closest('.navbar')) {
+      this.isMenuOpen = false;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(): void {
+    if (window.innerWidth > 992) {
+      this.isMenuOpen = false;
+    }
+  }
+}
