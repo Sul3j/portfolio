332878120bc658e13251f02c4e4d352244e6b0b7
Szymon Sulejczak <contact@szymonsulejczak.com>
2025-02-20 21:33:52 +0100

    add footer

--- portfolio_ui/src/app/app.component.ts
+++ portfolio_ui/src/app/app.component.ts
@@ -5,11 +5,12 @@ import { AboutComponent } from './about/about.component';
 import { TechnologiesComponent } from "./technologies/technologies.component";
 import { ProjectsComponent } from "./projects/projects.component";
 import { ContactComponent } from "./contact/contact.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, NavigationComponent, AboutComponent, TechnologiesComponent, ProjectsComponent, ContactComponent],
+  imports: [HeaderComponent, NavigationComponent, AboutComponent, TechnologiesComponent, ProjectsComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
