bbc03c8969adb28b6537d952b00c7e16751107a6
Szymon Sulejczak <contact@szymonsulejczak.com>
2025-02-20 16:34:35 +0100

    add technologies section styles

--- portfolio_ui/src/app/app.component.ts
+++ portfolio_ui/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { AboutComponent } from './about/about.component';
+import { TechnologiesComponent } from "./technologies/technologies.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, NavigationComponent, AboutComponent],
+  imports: [HeaderComponent, NavigationComponent, AboutComponent, TechnologiesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- portfolio_ui/src/app/technologies/technologies.component.ts
+++ portfolio_ui/src/app/technologies/technologies.component.ts
@@ -1,11 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-technologies',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './technologies.component.html',
   styleUrl: './technologies.component.scss'
 })
 export class TechnologiesComponent {
-
+  public classes: string[] = [
+    "devicon-django-plain", 
+    "devicon-angular-plain", 
+    "devicon-python-plain", 
+    "devicon-javascript-plain",
+    "devicon-typescript-plain",
+    "devicon-nodejs-plain-wordmark",
+    "devicon-nestjs-plain",
+    "devicon-sass-plain",
+    "devicon-docker-plain",
+    "devicon-npm-original-wordmark",
+    "devicon-figma-plain",
+    "devicon-vscode-plain"
+  ];
 }
