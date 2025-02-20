67892201db163e6ae3f3ef171aaee9740cd36a35
Szymon Sulejczak <contact@szymonsulejczak.com>
2025-02-20 20:20:52 +0100

    add contact section structure

--- portfolio_ui/src/app/app.component.ts
+++ portfolio_ui/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { AboutComponent } from './about/about.component';
 import { TechnologiesComponent } from "./technologies/technologies.component";
 import { ProjectsComponent } from "./projects/projects.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, NavigationComponent, AboutComponent, TechnologiesComponent, ProjectsComponent],
+  imports: [HeaderComponent, NavigationComponent, AboutComponent, TechnologiesComponent, ProjectsComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- portfolio_ui/src/app/header/header.component.ts
+++ portfolio_ui/src/app/header/header.component.ts
@@ -10,9 +10,9 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   socials = [
-    { icon: 'fas fa-envelope', link: '' },
-    { icon: 'fab fa-gitlab', link: '' },
-    { icon: 'fab fa-github', link: '' },
-    { icon: 'fab fa-linkedin', link: '' }
+    { icon: 'fas fa-envelope', link: 'mailto:contact@szymonsulejczak.com' },
+    { icon: 'fab fa-github', link: 'https://github.com/Sul3j' },
+    { icon: 'fab fa-gitlab', link: 'https://gitlab.com/szymon.sul3jczak' },
+    { icon: 'fab fa-linkedin', link: 'https://www.linkedin.com/in/szymon-sulejczak-1984451b7/' }
   ];
 }
