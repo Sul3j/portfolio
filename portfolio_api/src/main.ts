9a9fc0bde4d480fd95d16baab596421e26e078be
Szymon Sulejczak <contact@szymonsulejczak.com>
2025-02-21 19:44:38 +0100

    add email send logic

--- portfolio_api/src/main.ts
+++ portfolio_api/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: 'https://szymonsulejczak.com',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
--- portfolio_ui/src/app/contact/contact.component.ts
+++ portfolio_ui/src/app/contact/contact.component.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+  contactForm: FormGroup;
+  successMessage: string = '';
+  errorMessage: string = '';
 
+  constructor(private fb: FormBuilder, private http: HttpClient, private toastr: ToastrService) {
+    this.contactForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
+  sendEmail() {
+    if (this.contactForm.invalid) return;
+
+    this.http.post('https://szymonsulejczak.com/email/send', this.contactForm.value).subscribe({
+      next: () => {
+        this.successMessage = 'Wiadomość wysłana pomyślnie!';
+        this.errorMessage = '';
+        this.contactForm.reset();
+        this.toastr.success(this.successMessage);
+      },
+      error: () => {
+        this.errorMessage = 'Błąd podczas wysyłania wiadomości!';
+        this.successMessage = '';
+        this.toastr.error(this.errorMessage);
+      }
+    });
+  }
 }
